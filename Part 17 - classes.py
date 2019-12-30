55cc943301661285dc02c9e6c9a8784cba4978a2
aravindakumar79 <37686588+aravindakumar79@users.noreply.github.com>
2019-12-30 12:32:44 +0530

    Classes Python code

---
+++ Part 17 - classes.py
@@ -0,0 +1,19 @@
+
+
+class calc:
+
+    def add(x,y):
+        answer = x + y
+        print(answer)
+
+    def sub(x,y):
+        answer = x - y
+        print(answer)
+
+    def mult(x,y):
+        answer = x * y
+        print(answer)
+
+    def div(x,y):
+        answer = x / y
+        print(answer)
